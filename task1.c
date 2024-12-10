ce06a42f32329d82eed58d3fb1f1fa5672715282
IkramovAmir <amirikromov30@gmail.com>
2024-12-10 10:27:34 +0500

    Fixed

---
+++ task1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+int main(){
+    char word[20];
+    int count;
+    
+    printf("Enter information: ");
+    fgets(word,sizeof(word), stdin);
+
+    for (int i = 0; word[i] != '\0'; i++)
+    {
+        if (isupper(word[i]))
+        {
+            count++;
+        }
+        
+    }
+
+    printf("There are %d uppercases", count);
+    return 0;
+}
